c0ac36b3c945ff70561b2ee6507ec4ac7c6d0ad7
MegaScooper <rz20dz@student.aau.dk>
2022-11-28 15:54:39 +0100

    Started setting up main and header/side. Need to setup some storeIDs to setup tests/trial versions before possible end getting store IDs from location

--- src/coopapi.c
+++ src/coopapi.c
@@ -1,3 +1,24 @@
 //
 // Created by carl on 28/11/2022.
 //
+#include <stdio.h>
+#include <string.h>
+#include <curl/curl.h>
+int main(int argc, char *argv[]){
+    CURL *curl;
+    CURLcode res;
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.cl.coop.dk/productapi/v1/product/1290");
+        curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https");
+        struct curl_slist *headers = NULL;
+        headers = curl_slist_append(headers, "Ocp-Apim-Subscription-Key: 443332aed9904c8f902861f77ad68d42");
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        /* disables SSL security, very bad, but as of know we got no clue how to fix our SSL or SSH problem*/
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+        res = curl_easy_perform(curl);
+    }
+    curl_easy_cleanup(curl);
+    return (int)res;
+}
--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
+#include "coopapi.h"
 #include <stdio.h>
+#include <string.h>
+#include <curl/curl.h>
+typedef enum storeid{"fakta"=1290, "Dagli'brugsen"=1043} storeid;
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]){
+    char coopurl[100] = "https://api.cl.coop.dk/productapi/v1/product/";
+    char coopapikey[50];
+
+    printf("Input API key: ");
+    scanf("%s", coopapikey);
+    char *str = malloc(sizeof(char)*10000);
+    if (str==NULL){
+        perror("Could not create big string!");
+        exit(1);
+    }
+
+
+    coopapi_to_itemstr(url, "https", str);
+
+    printf("%s\n\n", str);
+
     return 0;
 }
