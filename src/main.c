a3f0668688d25537dafcdf44ce83b67e3d3449df
MegaScooper <rz20dz@student.aau.dk>
2022-11-29 11:26:36 +0100

    StoreIDs for Daglibrugsen and Coop365. Needs to actually do something.

--- src/main.c
+++ src/main.c
@@ -2,12 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <curl/curl.h>
-typedef enum storeid{"fakta"=1290, "Dagli'brugsen"=1043} storeid;
+
+//Enum only for Daglibrugsen and Coop365, because it's the only working Coop api storeIDs
+enum storeNameToID{Daglibrugsen=0, Coop365};
 
 int main(int argc, char *argv[]){
     char coopurl[100] = "https://api.cl.coop.dk/productapi/v1/product/";
     char coopapikey[50];
-
+    int storeName;
+    char *storeID = malloc(sizeof(char)*10000);
+    switch (storeName) {
+        case 0:
+            strcpy(storeID, "1290");
+            break;
+        case 1:
+            strcpy(storeID, "24165");
+            break;
+        case 2:
+    }
     printf("Input API key: ");
     scanf("%s", coopapikey);
     char *str = malloc(sizeof(char)*10000);
