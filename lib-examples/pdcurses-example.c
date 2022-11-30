cb318d3e8d333c9869a48b90468c08f695cbdab0
Jet8100 <YN62DD@student.aau.dk>
2022-11-30 20:42:04 +0100

    Update pdcurses-example.c

--- lib-examples/pdcurses-example.c
+++ lib-examples/pdcurses-example.c
@@ -1,9 +1,21 @@
+#include <stdio.h>
 #include <curses.h>
 
 int main() {
     initscr(); // Start curses mode
-    mvprintw(10, 10, "Hello, Curses!");
-    refresh(); // Update screen
+    noecho();
+    cbreak();
+
+    int yMax, xMax;
+    getmaxyx(stdscr, yMax, xMax);
+
+    WINDOW * inputwin = newwin(6, xMax-12, yMax-8, 5);
+    box(inputwin,0,0);
+    refresh();
+    wrefresh(inputwin);
+    keypad(inputwin, true);
+
+    //mvprintw(10, 10, "\U0001F600");
     getchar();
     endwin(); // End curses mode
     return 0;
