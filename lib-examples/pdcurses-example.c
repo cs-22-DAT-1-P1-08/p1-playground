a331c51a2204eb106d11b07acd66624b52db0156
wesam1506 <113096079+IO32NQ@users.noreply.github.com>
2022-12-04 21:33:19 +0100

    Update pdcurses-example.c

--- lib-examples/pdcurses-example.c
+++ lib-examples/pdcurses-example.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <stdlib.h>
 
+//a struct for fake data
 typedef struct {
     char product_name[30];
     char store[20];
@@ -11,84 +12,91 @@ typedef struct {
     int distance;
 } demo_products;
 
-int rnd_nr(int size)
-{
-    return rand() % size;
-}
-
-int check_longest_word(char* test, char* test2)
+//Checks for the longest product name and changes the size of the box
+int check_longest_word(demo_products products2[], int size)
 {
     int result;
-    int maxLength = 0;
+    int maxLength = 9;
+    char* word;
 
-    if (strlen(test) > strlen(test2))
-        maxLength = strlen(test);
-    else if (strlen(test) < strlen(test2))
-        maxLength = strlen(test2);
+    for (int i = 0; i < size; ++i)
+    {
+        word = products2[i].product_name;
+        if (strlen(word) > maxLength)
+            maxLength = strlen(word);
 
-    result = maxLength;
+        result = maxLength;
+    }
 
     return result;
 }
 
-int space(char* test, char* test2)
-{
-    int number;
-    for (int i = 0; i < strlen(test)-strlen(test2)+1; ++i) {
-        number++;
-    }
-    return number;
-}
-
-void lines(char* test, int row)
+//Changes the size of the line depending on the longest word
+void lines(demo_products products2[], int row, int size)
 {
     mvprintw(row, 10, "+--");
-    for (int i = 0; i < strlen(test); ++i) {
+    for (int i = 0; i < check_longest_word(products2, size); ++i) {
         mvprintw(row, 12+i,"-");
     }
 }
 
+//Prints the shopping list
 void print_shopping_list(char* test, demo_products products2[], int size)
 {
-    int number;
     double total_price1;
     double total_price2;
+    char* word1 = products2[rand() % size].store;
+    char* word2 = products2[rand() % size].store;
+    int store_space1 = (17-(strlen(word1)+6))/2;
+    int store_space2 = (17-(strlen(word2)+6))/2;
 
-    mvprintw(0, 30, "Grocery list");
+    mvprintw(0, 15+check_longest_word(products2, size)+store_space1, "Grocery list");
 
-    lines(test,1);
+    //The store row
+    lines(products2,1,size);
+    mvprintw(1, 12+check_longest_word(products2, size), "+--------------------");
+    mvprintw(1, 15+check_longest_word(products2, size)+store_space1, " %s (0) ", word1);
+    mvprintw(1, 33+check_longest_word(products2, size), "+--------------------");
+    mvprintw(1, 36+check_longest_word(products2, size)+store_space2, " %s (1) ", word2);
+    mvprintw(1, 54+check_longest_word(products2, size), "+");
 
-    mvprintw(1, 12+strlen(test), "+--- %s (0) ---+---- %s (1) ----+\n", products2[rnd_nr(size)].store, products2[rnd_nr(size)].store);
-
+    //The name of the items, price and weight/amount row
     int i = 0;
     while (i < size-1) //Minus 1 because we don't want "q" to be there
     {
-        mvprintw(2+i, 10, "|%s | %.2lf kr (%s)    | %.2lf kr (%s)       |", products2[i].product_name, products2[i].price, products2[i].amount, products2[i+1].price, products2[i].amount);
+        mvprintw(2+i, 10, "|%s ", products2[i].product_name);
+        mvprintw(2+i, 12 + check_longest_word(products2, size), "| %.2lf kr (%s) ", products2[i].price, products2[i].amount);
+        mvprintw(2+i, 33 + check_longest_word(products2, size), "| %.2lf kr (%s) ", products2[i+1].price, products2[i].amount);
+        mvprintw(2+i, 54 + check_longest_word(products2, size), "|");
 
         total_price1 = total_price1 + products2[i].price;
         total_price2 = total_price2 + products2[i+1].price;
         i++;
     }
 
-    lines(test,2+i);
-    mvprintw(2+i, 12+strlen(test), "+------------------+--------------------+\n");
+    lines(products2,2+i,size);
+    mvprintw(2+i, 12+check_longest_word(products2, size), "+--------------------+--------------------+");
 
+    //The total price row
     mvprintw(3+i, 10, "|Total");
-    number = strlen("|Distance") + space(test, "Distance") + 1;
-    mvprintw(3+i, number, "| %.2lf kr         | %.2lf kr           |\n", total_price1, total_price2);
+    mvprintw(3+i, 12 + check_longest_word(products2, size), "| %.2lf kr ", total_price1);
+    mvprintw(3+i, 33 + check_longest_word(products2, size), "| %.2lf kr ", total_price2);
+    mvprintw(3+i, 54 + check_longest_word(products2, size), "|");
 
-    lines(test,4+i);
-    mvprintw(4+i, 12+strlen(test), "+------------------+--------------------+\n\n");
+    lines(products2,4+i,size);
+    mvprintw(4+i, 12+check_longest_word(products2, size), "+--------------------+--------------------+");
 
+    //The distance row
     mvprintw(5+i, 10, "|Distance");
-    number = strlen("|Distance") + space(test, "Distance") + 1;
-    mvprintw(5+i, number, "| %d meters away  | %d meters away   |\n", products2[rnd_nr(size)].distance, products2[rnd_nr(size)].distance);
+    mvprintw(5+i, 12 + check_longest_word(products2, size), "| %d meters away ", products2[rand() % size].distance);
+    mvprintw(5+i, 33 + check_longest_word(products2, size), "| %d meters away ", products2[rand() % size].distance);
+    mvprintw(5+i, 54 + check_longest_word(products2, size), "|");
 
-    lines(test,6+i);
-    mvprintw(6+i, 12+strlen(test), "+------------------+--------------------+\n\n");
+    lines(products2,6+i,size);
+    mvprintw(6+i, 12+check_longest_word(products2, size), "+--------------------+--------------------+\n\n");
 }
 
-int main()
+int ui()
 {
     //int size = sizeof(products1) / sizeof(products1[0]);
 
@@ -104,9 +112,10 @@ int main()
 
     demo_products products1[40];
 
+    //Filling the struct with demo/fake data
     char* demo_store[] = {"Netto", "Fakta", "Aldi", "Lidl", "Rema 1000", "Foetex", "Irma", "SuperBrugsen", "Kvickly", "Spar"};
-    char* demo_weight[] = {"1L", "250g", "500g", "1kg", "5dl", "2kg", "2L", "7dl", "25g", "800g"};
-    double demo_price[] = {30.95, 40.00, 12.99, 19.00, 22.50, 49.50, 120.00, 10.00, 7.49, 150};
+    char* demo_weight[] = {"999g","1L", "250g", "500g", "1kg", "5dl", "2kg", "2L", "7dl", "25g"};
+    double demo_price[] = {9999.50,9998.95, 40.00, 12.99, 19.00, 22.50, 49.50, 120.00, 10.00, 7.49};
     int demo_distance[] = {551, 181, 2224, 752, 42, 576, 1296, 392, 226, 127};
 
     initscr(); // Start curses mode
@@ -126,12 +135,18 @@ int main()
         size++;
     }
 
-    clear();
+    clear(); //Clears the screen
 
     print_shopping_list(name_test1, products1, size);
 
     refresh(); // Update screen
     getchar();
     endwin(); // End curses mode
+    return 0;
+}
+
+int main()
+{
+    ui();
     return 0;
 }
