56a684460e3b638513a693e7bb788cc2ff646063
7H30D0R <marcusanholt@gmail.com>
2022-12-05 10:43:24 +0100

    Combine data from Coop API and Tjek API

--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@ void coop_api_temp(void);
 void tjek_api_temp();
 
 int main() {
+    printf("Started program...\n");
     store_t *daglibrugsen = get_coop_store("1290", DB_DEALER_ID);
     store_t *coop365 = get_coop_store("24165", COOP365_DEALER_ID);
 
@@ -26,6 +27,30 @@ void print_products(store_t *store) {
     dlist_node_t *item_node = store->products->head;
     while (item_node != NULL) {
         item_t *item = item_node->data;
+        if (item == NULL)
+            continue;
+
+        printf("[%s] %s (%.2lf kr)", item->ean, item->name, item->price);
+        if (item->amount != NULL)
+            printf(" %.2lf %s", item->amount->amount, get_unit_name(item->amount->unit_type));
+        if (item->offer != NULL)
+            printf("\n ---------- TILBUD /\\: %.2lf kr ------------- ", item->offer->group->price);
+        printf("\n");
+
+        item_node = item_node->next;
+    }
+}
+
+void coop_api_temp(void) {
+    /* Daglibrugsen */
+    dlist_t* list_dagligbrugsen = coop_get_items("1290");
+
+    /* coop 365 */
+    dlist_t* list_coop365 = coop_get_items("24165");
+
+    dlist_node_t *item_node = list_dagligbrugsen->head;
+    while (item_node != NULL) {
+        item_t *item = item_node->data;
         if (item->amount != NULL) {
             printf("%s: %.2lf %s\n", item->name, item->amount->amount, get_unit_name(item->amount->unit_type));
         }
@@ -33,48 +58,29 @@ void print_products(store_t *store) {
         item_node = item_node->next;
     }
 
-    void coop_api_temp(void){
-        /* Daglibrugsen */
-        dlist_t* list_dagligbrugsen = coop_get_items("1290");
-
-        /* coop 365 */
-        dlist_t* list_coop365 = coop_get_items("24165");
-
-        dlist_node_t *item_node = list_dagligbrugsen->head;
-        while (item_node != NULL) {
-            item_t *item = item_node->data;
-            if (item->amount != NULL) {
-                printf("%s: %.2lf %s\n", item->name, item->amount->amount, get_unit_name(item->amount->unit_type));
-            }
-
-            item_node = item_node->next;
-        }
-
-        dlist_free_all(list_dagligbrugsen, (void (*)(void *)) free_item);
-        dlist_free_all(list_coop365, (void (*)(void *)) free_item);
-    }
-
-    void tjek_api_temp() {
-        catalog_info_t *db_catalog_info = get_catalog_info(COOP365_DEALER_ID);
-        printf("%s: %d\n", db_catalog_info->id, db_catalog_info->offer_count);
-
-        catalog_offers_t *catalog_offers = get_catalog_offers(db_catalog_info);
-
-        dlist_node_t *offer_node = catalog_offers->offers->head;
-        while (offer_node != NULL) {
-            offer_t *offer = (offer_t*)offer_node->data;
-            if (offer->group == NULL) {
-                printf("Something is rotten in the state of Denmark...\n");
-                offer_node = offer_node->next;
-                continue;
-            }
-            printf("%.2lf kr, %s [%s]\n", offer->group->price, offer->title, offer->ean);
-            offer_node = offer_node->next;
-        }
-
-        free_catalog_offers(catalog_offers);
-        free_catalog_info(db_catalog_info);
-    }
-
-
+    dlist_free_all(list_dagligbrugsen, (void (*)(void *)) free_item);
+    dlist_free_all(list_coop365, (void (*)(void *)) free_item);
 }
+
+void tjek_api_temp() {
+    catalog_info_t *db_catalog_info = get_catalog_info(COOP365_DEALER_ID);
+    printf("%s: %d\n", db_catalog_info->id, db_catalog_info->offer_count);
+
+    catalog_offers_t *catalog_offers = get_catalog_offers(db_catalog_info);
+
+    dlist_node_t *offer_node = catalog_offers->offers->head;
+    while (offer_node != NULL) {
+        offer_t *offer = (offer_t*)offer_node->data;
+        if (offer->group == NULL) {
+            printf("Something is rotten in the state of Denmark...\n");
+            offer_node = offer_node->next;
+            continue;
+        }
+        printf("%.2lf kr, %s [%s]\n", offer->group->price, offer->title, offer->ean);
+        offer_node = offer_node->next;
+    }
+
+    free_catalog_offers(catalog_offers);
+    free_catalog_info(db_catalog_info);
+}
+
--- src/store.c
+++ src/store.c
@@ -1,13 +1,34 @@
 #include "store.h"
 #include <stdlib.h>
+#include <string.h>
 #include "api/coop_api.h"
 #include "api/tjek_api.h"
+
+store_t link_offer_data(store_t* store) {
+    dlist_node_t *item_node = store->products->head;
+    while (item_node != NULL) {
+        item_t *item = item_node->data;
+        if (item == NULL)
+            continue;
+
+        dlist_node_t *offer_node = store->offers->offers->head;
+        while (offer_node != NULL) {
+            offer_t *offer = offer_node->data;
+            if (offer != NULL && strcmp(offer->ean, item->ean) == 0)
+                item->offer = offer;
+            offer_node = offer_node->next;
+        }
+
+        item_node = item_node->next;
+    }
+}
 
 store_t* get_coop_store(char* store_id, char* dealer_id) {
     store_t *store = calloc(1, sizeof(store_t));
     store->products = coop_get_items(store_id);
     store->catalog_info = get_catalog_info(dealer_id);
     store->offers = get_catalog_offers(store->catalog_info);
+    link_offer_data(store);
     return store;
 }
 
