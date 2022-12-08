1dd27b27ef0bc37acd0de4721a7707c36847c26f
ltalexandre <ltalexandre@hotmail.com>
2022-12-08 10:27:36 +0100

    Sort and search implementation in Coop API

--- src/main.c
+++ src/main.c
@@ -7,8 +7,18 @@ int main() {
     printf("Started program...\n");
     store_t *daglibrugsen = get_coop_store("1290", DB_DEALER_ID);
     store_t *coop365 = get_coop_store("24165", COOP365_DEALER_ID);
+    store_t stores[] = {*daglibrugsen, *coop365};
+    //char *search_term[] = {"AGURK", "TOMAT"};
     // Grænseflade implementation
-    printf("%s: %lf", find_cheapest_match(daglibrugsen, "TUBORG").name,find_cheapest_match(daglibrugsen, "TUBORG").price);
+    char *search_term = "AGURK";
+
+    item_t *cheapest_store_item  = find_cheapest_match(stores, search_term);
+    printf("%s: %lf\n", cheapest_store_item->name, cheapest_store_item->price);
+
+    item_t *cheapest_overall_item = compare_item(stores, search_term, 2);
+    printf("%s: %lf", cheapest_overall_item->name, cheapest_overall_item->price);
+
+    //printf("COOP365 %s: %lf", find_cheapest_match(coop365, "AGURK").name,find_cheapest_match(coop365, "AGURK").price);
     free_store(daglibrugsen);
     free_store(coop365);
     return 0;
--- src/sorting_functions.c
+++ src/sorting_functions.c
@@ -11,16 +11,16 @@ int cmp_item_price(item_t *a, item_t *b) {
     return (int)(a->price - b->price);
 }
 
-item_t find_cheapest_match(store_t *store, char* search_term) {
-    item_t results;
+item_t *find_cheapest_match(store_t *store, char *search_term) {
+    item_t *results;
     dlist_node_t *item_node = store->items->head;
     while (item_node != NULL) {
         item_t *item = item_node->data;
         // TODO: make string comparison case insensitive
         if (item != NULL && strstr(item->name, search_term) != NULL) {
-            // compares "results" with they new item, which has the same string
-            if (cmp_item_price(&results, item)) {
-                results = *item;
+            // compares "results" with they knew item, which has the same string
+            if (cmp_item_price(results, item)) {
+                *results = *item;
             }
         }
         item_node = item_node->next;
@@ -28,3 +28,28 @@ item_t find_cheapest_match(store_t *store, char* search_term) {
     //returns the result
     return results;
 }
+
+item_t *compare_item(store_t stores[], char *search_term, size_t length_of_store)
+{
+    item_t *cheapest_item;
+    for (int i = 0; i < length_of_store; ++i) {
+        dlist_node_t *item_node = stores[i].items->head;
+        item_t *item = item_node->data;
+        if (item != NULL && strstr(item->name, search_term) != NULL) {
+            item_t *temp_match = find_cheapest_match(&stores[i], search_term);
+            if (cmp_item_price(cheapest_item, temp_match)) {
+                *cheapest_item = *temp_match;
+            }
+        }
+    }
+
+    return cheapest_item;
+}
+
+/*void print_items(store_t stores[], char *search_term)
+{
+    for (int i = 0; i < stores; ++i) {
+        printf("%s: %lf", find_cheapest_match(&stores[i], search_term) );
+    }
+    printf("%s: %lf", compare_stores(stores, search_term).item, compare_stores(stores).price);
+}*/
